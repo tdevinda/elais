3b09f3673ef2b717d2fd2a41436c295ae3d9b4e2
Tharaka <tharaka@vista.aero>
2013-11-18 01:45:46 +0530

    Now able to use Calculations from LVP and list stations within 10*1.4142 km radius

--- src/lk/tharu/elais/ActivityStations.java
+++ src/lk/tharu/elais/ActivityStations.java
@@ -1,7 +1,5 @@
 package lk.tharu.elais;
 
-import com.lvp.ListViewPopulator;
-
 import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
@@ -10,6 +8,12 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
+
+import com.lvp.Calculation;
+import com.lvp.Calculation.OPERAND;
+import com.lvp.Element;
+import com.lvp.Element.TYPE;
+import com.lvp.ListViewPopulator;
 
 public class ActivityStations extends Activity {
 	
@@ -27,23 +31,33 @@ public class ActivityStations extends Activity {
 		lvp.setListViewEntryLayoutID(R.layout.entry_station);
 		lvp.setMapping(R.id.text_entryStation_name, ModuleDatabase.col_stationName);
 		lvp.setMapping(R.id.text_entryStation_area, ModuleDatabase.col_stationArea);
+		lvp.setMapping(0, ModuleDatabase.col_stationLatitute);
+		lvp.setMapping(0, ModuleDatabase.col_stationLongitude);
+		
 		
 		
 		final LocationManager locationManager = (LocationManager) 
 				getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
 		
 		
-		Location dummyLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+		Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+		//dummyLocation.setLatitude(6.75);
+		//dummyLocation.setLongitude(79.89);
+		
+		
+		
+		lvp.setCalculation(getDistanceCalculation(lastKnownLocation, 0.1), R.id.text_entryStation_distance);
+		
 		double latlonDifference = 0.1;
 		lvp.populate(
 				ModuleDatabase.col_stationLatitute +" > ? and "+ ModuleDatabase.col_stationLatitute +" < ? and "+
 				ModuleDatabase.col_stationLongitude +" > ? and "+ ModuleDatabase.col_stationLongitude +" < ?",
 
 				new String[] {
-					Double.toString(dummyLocation.getLatitude() - latlonDifference),
-					Double.toString(dummyLocation.getLatitude() + latlonDifference),
-					Double.toString(dummyLocation.getLongitude() - latlonDifference),
-					Double.toString(dummyLocation.getLongitude() + latlonDifference),
+					Double.toString(lastKnownLocation.getLatitude() - latlonDifference),
+					Double.toString(lastKnownLocation.getLatitude() + latlonDifference),
+					Double.toString(lastKnownLocation.getLongitude() - latlonDifference),
+					Double.toString(lastKnownLocation.getLongitude() + latlonDifference)
 				}
 		);
 		
@@ -74,26 +88,20 @@ public class ActivityStations extends Activity {
 				Log.v("elais", "new location");
 				
 				//TODO populate the stations here.
-				int defaultStationRange = 10;
-				long latlonDifference = defaultStationRange / 100;
-				Location dummyLocation = new Location(location);
-				dummyLocation.setLatitude(6.75);
-				dummyLocation.setLongitude(79.89);
+				double defaultStationRange = 10.0;
+				double latlonDifference = defaultStationRange / 100;
 				
 				lvp.populate(
 						ModuleDatabase.col_stationLatitute +" > ? and "+ ModuleDatabase.col_stationLatitute +" < ? and "+
 						ModuleDatabase.col_stationLongitude +" > ? and "+ ModuleDatabase.col_stationLongitude +" < ?",
 						
 						new String[] {
-								Double.toString(dummyLocation.getLatitude() - latlonDifference),
-								Double.toString(dummyLocation.getLatitude() + latlonDifference),
-								Double.toString(dummyLocation.getLongitude() - latlonDifference),
-								Double.toString(dummyLocation.getLongitude() + latlonDifference),
-						});
-				
-				Log.v("elais", "new location from " + location.getProvider() + 
-						" lat="+ location.getLatitude() + " lon="+ location.getLongitude() +
-						" accuracy="+ location.getAccuracy());
+								Double.toString(location.getLatitude() - latlonDifference),
+								Double.toString(location.getLatitude() + latlonDifference),
+								Double.toString(location.getLongitude() - latlonDifference),
+								Double.toString(location.getLongitude() + latlonDifference)
+						}
+						);
 				
 				if(location.getAccuracy() < 100) {
 					locationManager.removeUpdates(this);
@@ -113,5 +121,63 @@ public class ActivityStations extends Activity {
 		if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
 			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationUpdateListener);
 		} */
+	}
+	
+	
+	private Calculation getDistanceCalculation(Location location, double range) {
+
+		
+		//x = (dbLat - currentLat) ^ 2 
+		Calculation latSq = new Calculation(
+				new Element(new Calculation(
+						new Element(ModuleDatabase.col_stationLatitute, TYPE.DB_COLUMN),
+						OPERAND.SUBTRACT,
+						new Element(location.getLatitude(), TYPE.NUMBER)
+						), TYPE.CALCULATION), 
+						OPERAND.POWER, 
+						new Element(2, TYPE.NUMBER)
+				);
+
+		//y = (dbLon - currentLon) ^ 2
+		Calculation lonSq = new Calculation(
+				new Element(new Calculation(
+						new Element(ModuleDatabase.col_stationLongitude, TYPE.DB_COLUMN),
+						OPERAND.SUBTRACT,
+						new Element(location.getLongitude(), TYPE.NUMBER)
+						), TYPE.CALCULATION), 
+						OPERAND.POWER, 
+						new Element(2, TYPE.NUMBER)
+				);
+		// x + y
+		Calculation added = new Calculation(
+				new Element(latSq, TYPE.CALCULATION),
+				OPERAND.ADD,
+				new Element(lonSq, TYPE.CALCULATION)
+				);
+
+		//z=  sqrt(x + y)
+		Calculation latondiff = new Calculation(
+				new Element(added, TYPE.CALCULATION),
+				OPERAND.ROOT,
+				new Element(2, TYPE.NUMBER)
+				);
+
+		// round (z)
+		Calculation roundedDifference = new Calculation(
+				new Element(new Calculation(
+						new Element(latondiff, TYPE.CALCULATION),
+						OPERAND.MULTIPLY,
+						new Element(100, TYPE.NUMBER)
+						), TYPE.CALCULATION), 
+						OPERAND.ROUND, 
+						new Element(0, TYPE.NUMBER));
+		//value = z + " km"
+		Calculation displayed = new Calculation(
+				new Element(roundedDifference, TYPE.CALCULATION),
+				OPERAND.CONCAT,
+				new Element(" km", TYPE.NUMBER)
+				);
+
+		return displayed;
 	}
 }
